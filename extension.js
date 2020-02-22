89e66cc418ebf925614774574d5b7cd8e468fa41
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-02-22 20:59:57 +0200

    Implement ctags index

--- extension.js
+++ extension.js
@@ -1,37 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const readline = require('readline');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// [2020-02-22 17:34:57.024] [exthost] [warning] [Deprecation Warning] 'workspace.rootPath' is deprecated. Please use 'workspace.workspaceFolders' instead. More details: https://aka.ms/vscode-eliminating-rootpath
 
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+    let disposable = vscode.commands.registerCommand('ctags-companion.reindex', function () {
+        vscode.window.showInformationMessage("Ctags Companion: reindexing...");
+        reindex();
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ctags-companion" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
+
+function reindex() {
+    const input = fs.createReadStream(vscode.workspace.rootPath + "/.tags");
+    const reader = readline.createInterface({ input, terminal: false, crlfDelay: Infinity })
+
+    const index = {};
+
+    reader.on("line", (line) => {
+        if (line.startsWith("!"))
+            return;
+
+        const [name, file, code] = line.split("\t");
+        if (!index.hasOwnProperty(name))
+            index[name] = [];
+        index[name].push({ file, code });
+    });
+
+    reader.on("close", () => {
+        vscode.window.showInformationMessage("Ctags Companion: reindex complete!");
+        console.log(`Index size: ${Object.keys(index).length}`);
+    });
+}
+
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {}
-
 module.exports = {
-	activate,
-	deactivate
+    activate
 }
