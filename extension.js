88eeaadffc55a4b501b375a3e3492ee41d647c18
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-02-27 22:07:50 +0200

    Implement DocumentSymbolProvider

--- extension.js
+++ extension.js
@@ -4,10 +4,9 @@ const readline = require('readline');
 
 // TODO
 // [2020-02-22 17:34:57.024] [exthost] [warning] [Deprecation Warning] 'workspace.rootPath' is deprecated. Please use 'workspace.workspaceFolders' instead. More details: https://aka.ms/vscode-eliminating-rootpath
-// DocumentSymbolProvider
 // ctags on save
-// enable for languages
 // maintain multiple tag files: one for .venv (slow, ctagged once) and one for project (fast, ctagged on every file save), and merge them into a single index
+// enable for languages
 // SymbolInformation kind and containerName
 
 function activate(context) {
@@ -34,6 +33,31 @@ function activate(context) {
                     );
                 }
             }
+        )
+    );
+
+    context.subscriptions.push(
+        vscode.languages.registerDocumentSymbolProvider(
+            { scheme: "file" },
+            {
+                provideDocumentSymbols: async (document) => {
+                    const relativePath = vscode.workspace.asRelativePath(document.uri);
+                    const definitions = (await getDocumentIndex(context))[relativePath];
+                    if (!definitions) return;
+                    return definitions.map(({ symbol, file, line }) =>
+                        new vscode.SymbolInformation(
+                            symbol,
+                            null,
+                            null,
+                            new vscode.Location(
+                                vscode.Uri.file(vscode.workspace.rootPath + "/" + file),
+                                new vscode.Position(line, 0)
+                            )
+                        )
+                    );
+                }
+            },
+            { label: "Ctags Companion" }
         )
     );
 
@@ -88,11 +112,19 @@ async function getIndex(context) {
     return context.workspaceState.get("index");
 }
 
+async function getDocumentIndex(context) {
+    const index = context.workspaceState.get("documentIndex");
+    if (!index) await reindex(context);
+    return context.workspaceState.get("documentIndex");
+}
+
 function reindex(context) {
     return new Promise(resolve => {
         const input = fs.createReadStream(vscode.workspace.rootPath + "/.tags");
         const reader = readline.createInterface({ input, terminal: false, crlfDelay: Infinity });
+
         const index = {};
+        const documentIndex = {};
 
         reader.on("line", (line) => {
             if (line.startsWith("!")) return;
@@ -100,14 +132,19 @@ function reindex(context) {
             const [symbol, file, ...rest] = line.split("\t");
             const lineNumberStr = rest.find(value => value.startsWith("line:")).substring(5);
             const lineNumber = parseInt(lineNumberStr, 10) - 1;
+            const definition = { symbol, file, line: lineNumber };
 
             if (!index.hasOwnProperty(symbol)) index[symbol] = [];
-            index[symbol].push({ symbol, file, line: lineNumber });
+            index[symbol].push(definition);
+
+            if (!documentIndex.hasOwnProperty(file)) documentIndex[file] = [];
+            documentIndex[file].push(definition);
         });
 
         reader.on("close", () => {
             vscode.window.showInformationMessage("Ctags Companion: reindex complete!");
             context.workspaceState.update("index", index);
+            context.workspaceState.update("documentIndex", documentIndex);
             resolve();
         });
     });
