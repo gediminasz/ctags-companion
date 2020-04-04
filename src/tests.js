6e932e46a1832b10f237bc9ed1a4632c57237ef2
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-04-04 15:40:25 +0300

    Add tests for CtagsDocumentSymbolProvider

--- src/tests.js
+++ src/tests.js
@@ -2,12 +2,14 @@ const path = require('path');
 const vscode = require("vscode");
 
 const { CtagsDefinitionProvider } = require("./providers/ctags_definition_provider");
+const { CtagsDocumentSymbolProvider } = require("./providers/ctags_document_symbol_provider");
 
 async function runTests(context) {
     console.log("Running tests...");
     const document = await vscode.workspace.openTextDocument(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, "source.py"));
 
     testCtagsDefinitionProvider(context, document);
+    testCtagsDocumentSymbolProvider(context, document);
 }
 
 async function testCtagsDefinitionProvider(context, document) {
@@ -34,13 +36,44 @@ async function testCtagsDefinitionProvider(context, document) {
     assert(() => methodDefinition.range.start.line === 8);
 }
 
+async function testCtagsDocumentSymbolProvider(context, document) {
+    const provider = new CtagsDocumentSymbolProvider(context);
+
+    const definitions = await provider.provideDocumentSymbols(document);
+
+    console.log({ definitions });
+
+    assert(() => definitions.length == 4);
+
+    assert(() => definitions[0].name === "KONSTANT");
+    assert(() => definitions[0].kind === vscode.SymbolKind.Variable);
+    assert(() => definitions[0].location.uri.path.endsWith("source.py"));
+    assert(() => definitions[0].location.range.start.line === 0);
+
+    assert(() => definitions[1].name === "Klass");
+    assert(() => definitions[1].kind === vscode.SymbolKind.Class);
+    assert(() => definitions[1].location.uri.path.endsWith("source.py"));
+    assert(() => definitions[1].location.range.start.line === 7);
+
+    assert(() => definitions[2].name === "funktion");
+    assert(() => definitions[2].kind === vscode.SymbolKind.Function);
+    assert(() => definitions[2].location.uri.path.endsWith("source.py"));
+    assert(() => definitions[2].location.range.start.line === 3);
+
+    assert(() => definitions[3].name === "method");
+    assert(() => definitions[3].kind === vscode.SymbolKind.Method);
+    assert(() => definitions[3].location.uri.path.endsWith("source.py"));
+    assert(() => definitions[3].location.range.start.line === 8);
+}
+
+
+
 function assert(condition) {
-    console.count("test");
     if (condition()) {
         console.count("pass");
     } else {
         console.count("fail");
-        console.error("Assertion failed: ", condition.toString());
+        console.error("FAIL: ", condition.toString());
     }
 }
 
