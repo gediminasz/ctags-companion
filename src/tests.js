4243baae09d1f49d0f9dd46c080fa879f355e39a
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-04-04 16:45:12 +0300

    Add a test for when definition is not found

--- src/tests.js
+++ src/tests.js
@@ -17,25 +17,24 @@ async function runTests(context) {
 async function testCtagsDefinitionProvider(context, document) {
     const provider = new CtagsDefinitionProvider(context);
 
-    // KONSTANT
     const [konstantDefinition] = await provider.provideDefinition(document, new vscode.Position(9, 21));
     assert(() => konstantDefinition.uri.path.endsWith("source.py"));
     assert(() => konstantDefinition.range.start.line === 0);
 
-    // funktion
     const [funktionDefinition] = await provider.provideDefinition(document, new vscode.Position(9, 12));
     assert(() => funktionDefinition.uri.path.endsWith("source.py"));
     assert(() => funktionDefinition.range.start.line === 3);
 
-    // Klass
     const [klassDefinition] = await provider.provideDefinition(document, new vscode.Position(12, 3));
     assert(() => klassDefinition.uri.path.endsWith("source.py"));
     assert(() => klassDefinition.range.start.line === 7);
 
-    // method
     const [methodDefinition] = await provider.provideDefinition(document, new vscode.Position(12, 8));
     assert(() => methodDefinition.uri.path.endsWith("source.py"));
     assert(() => methodDefinition.range.start.line === 8);
+
+    const printDefinitions = await provider.provideDefinition(document, new vscode.Position(4, 7));
+    assert(() => printDefinitions === undefined);
 }
 
 async function testCtagsDocumentSymbolProvider(context, document) {
