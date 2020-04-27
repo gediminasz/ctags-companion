8c5721c2d22d5a4f801f33cb858df7e09eb0c340
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-04-27 21:54:14 +0300

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": { "ecmaVersion": 2018 },
+    "rules": {
+        "indent": ["error", 4, { "SwitchCase": 1 }],
+        "semi": ["error", "always"],
+        "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+        "no-prototype-builtins": "off"
+    }
+};
--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ module.exports = {
     EXTENSION_NAME: "Ctags Companion",
     EXTENSION_ID: "ctags-companion",
     TASK_NAME: "rebuild ctags",
-}
+};
