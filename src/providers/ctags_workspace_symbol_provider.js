750674fd60746afe6fca39cf5b88493380eb686f
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2021-12-15 23:12:25 +0200

    Use generators to loop over indexed Maps

--- src/providers/ctags_workspace_symbol_provider.js
+++ src/providers/ctags_workspace_symbol_provider.js
@@ -11,26 +11,29 @@ class CtagsWorkspaceSymbolProvider {
     provideWorkspaceSymbols(query) {
         if (!query) return;
 
-        const indexes = vscode.workspace.workspaceFolders.map(
-            scope => [scope, getIndexForScope(this.extension, scope)]
-        );
-
         const matcher = this.getMatcher(query);
 
-        return indexes.flatMap(([scope, { symbolIndex }]) => {
-            return [...symbolIndex]
-                .filter(([symbol]) => matcher(symbol.toLowerCase()))
-                .flatMap(([_, definitions]) => definitions)
-                .map(definition => definitionToSymbolInformation(definition, scope));
+        return vscode.workspace.workspaceFolders.flatMap(scope => {
+            const { symbolIndex } = getIndexForScope(this.extension, scope);
+            const definitions = this.findDefinitions(symbolIndex, matcher);
+            return Array.from(definitions).map(d => definitionToSymbolInformation(d, scope));
         });
+    }
+
+    *findDefinitions(symbolIndex, matcher) {
+        for (const [symbol, definitions] of symbolIndex) {
+            if (matcher(symbol)) {
+                yield* definitions;
+            }
+        }
     }
 
     getMatcher(query) {
         if (getConfiguration().get("fuzzyMatchingEnabled")) {
             const regexp = new RegExp('.*' + query.toLowerCase().split('').join('.*') + '.*');
-            return symbol => regexp.test(symbol);
+            return symbol => regexp.test(symbol.toLowerCase());
         } else {
-            return symbol => symbol.includes(query.toLowerCase());
+            return symbol => symbol.toLowerCase().includes(query.toLowerCase());
         }
     }
 }
