2024d853c76b0117c730df9320c60a6603e33154
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2026-01-31 14:23:57 +0200

    Refactor getCurrentWorkspaceScope

--- src/ctags.test.js
+++ src/ctags.test.js
@@ -2,36 +2,68 @@ const vscode = require('vscode');
 const { rebuildCtags } = require("./ctags");
 
 describe(rebuildCtags, () => {
-    it("runs ctags on the open file's workspace directory", () => {
-        vscode.window.activeTextEditor = { document: { uri: { fsPath: "/test/foo" } } };
-
+    it.each([undefined, []])("shows error when no workspace folders are open", (workspaceFolders) => {
+        vscode.workspace.workspaceFolders = workspaceFolders;
         const exec = jest.fn();
-        rebuildCtags(exec);
 
-        expect(exec.mock.calls.length).toBe(1);
-        expect(exec.mock.calls[0]).toStrictEqual(["mock-ctags-command", { cwd: "/test" }]);
-    });
-
-    it("runs ctags when there is only one workspace directory open", () => {
-        vscode.window.activeTextEditor = undefined;
-
-        const exec = jest.fn();
-        rebuildCtags(exec);
-
-        expect(exec.mock.calls.length).toBe(1);
-        expect(exec.mock.calls[0]).toStrictEqual(["mock-ctags-command", { cwd: "/test" }]);
-    });
-
-    it("shows error message when no file is open in a multi root workspace", () => {
-        vscode.window.activeTextEditor = undefined;
-        vscode.workspace.workspaceFolders = [{ uri: { fsPath: "/backend" } }, { uri: { fsPath: "/frontend" } }];
-
-        const exec = jest.fn();
         rebuildCtags(exec);
 
         expect(exec).not.toHaveBeenCalled();
-        expect(vscode.window.showErrorMessage).toHaveBeenCalledWith(
-            'Ctags Companion: Unable to determine active directory in a multi-root workspace. Please open some file and try again.'
-        );
+        expect(vscode.window.showErrorMessage).toHaveBeenCalledWith('Ctags Companion: No workspace folders open.');
+    });
+
+    it.each([
+        undefined,
+        { document: { uri: { fsPath: "/test/foo" } } },
+        { document: { uri: { fsPath: "/file/outside/workspace" } } },
+    ])("always runs ctags when there is a single folder in workspace", (activeTextEditor) => {
+        vscode.window.activeTextEditor = activeTextEditor;
+        vscode.workspace.workspaceFolders = [{ uri: { fsPath: "/test" } }];
+        const exec = jest.fn();
+
+        rebuildCtags(exec);
+
+        expect(exec).toHaveBeenCalledTimes(1);
+        expect(exec).toHaveBeenLastCalledWith("mock-ctags-command", { cwd: "/test" });
+    });
+
+    describe("when there are multiple folders in workspace", () => {
+        beforeEach(() => {
+            vscode.workspace.workspaceFolders = [{ uri: { fsPath: "/backend" } }, { uri: { fsPath: "/frontend" } }];
+        });
+
+        it("shows error when there is no active text editor", () => {
+            vscode.window.activeTextEditor = undefined;
+            const exec = jest.fn();
+
+            rebuildCtags(exec);
+
+            expect(exec).not.toHaveBeenCalled();
+            expect(vscode.window.showErrorMessage).toHaveBeenCalledWith(
+                'Ctags Companion: Unable to determine active directory in a multi-root workspace. Please open some file and try again.'
+            );
+        });
+
+        it("shows error when active text editor is outside of workspace", () => {
+            vscode.window.activeTextEditor = { document: { uri: { fsPath: "/file/outside/workspace" } } };
+            const exec = jest.fn();
+
+            rebuildCtags(exec);
+
+            expect(exec).not.toHaveBeenCalled();
+            expect(vscode.window.showErrorMessage).toHaveBeenCalledWith(
+                'Ctags Companion: Unable to determine active workspace directory for the currently open file.'
+            ); ``;
+        });
+
+        it("runs ctags on the open file's workspace directory", () => {
+            vscode.window.activeTextEditor = { document: { uri: { fsPath: "/test/foo" } } };
+            const exec = jest.fn();
+
+            rebuildCtags(exec);
+
+            expect(exec).toHaveBeenCalledTimes(1);
+            expect(exec).toHaveBeenLastCalledWith("mock-ctags-command", { cwd: "/test" });
+        });
     });
 });
