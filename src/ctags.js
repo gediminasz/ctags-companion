31479185aecb580feeccb4c516792c6e77ffb779
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2026-01-31 14:23:57 +0200

    Refactor getCurrentWorkspaceScope

--- src/ctags.js
+++ src/ctags.js
@@ -7,33 +7,44 @@ const { EXTENSION_NAME } = require("./constants");
  * @param {function} tryExec
  */
 function rebuildCtags(tryExec = helpers.tryExec) {
-    const scope = getCurrentWorkspaceScope();
-    if (scope === undefined) {
-        return;
+    try {
+        const scope = getCurrentWorkspaceScope();
+        const command = helpers.getConfiguration(scope).get("command");
+        const cwd = scope.uri.fsPath;
+        tryExec(command, { cwd });
+    } catch (error) {
+        vscode.window.showErrorMessage(`${EXTENSION_NAME}: ${error}`);
     }
-
-    const command = helpers.getConfiguration(scope).get("command");
-    const cwd = scope.uri.fsPath;
-
-    tryExec(command, { cwd });
 }
 
 /**
- * @returns {vscode.WorkspaceFolder | undefined}
+ * | folders in workspace | active file       | result |
+ * |----------------------|------------------ |--------|
+ * | 0                    | (any)             | error  |
+ * | 1                    | (any)             | OK     |
+ * | N                    | undefined         | error  |
+ * | N                    | outside workspace | error  |
+ * | N                    | within workspace  | OK     |
+ *
+ * @returns {vscode.WorkspaceFolder}
  */
 function getCurrentWorkspaceScope() {
-    if (vscode.window.activeTextEditor !== undefined) {
-        // TODO do not return undefined here
-        return vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor.document.uri);
-    }
-
-    if (vscode.workspace.workspaceFolders !== undefined && vscode.workspace.workspaceFolders.length === 1) {
+    if (vscode.workspace.workspaceFolders === undefined || vscode.workspace.workspaceFolders.length === 0) {
+        throw "No workspace folders open.";
+    } else if (vscode.workspace.workspaceFolders.length === 1) {
         return vscode.workspace.workspaceFolders[0];
+    } else {
+        if (vscode.window.activeTextEditor === undefined) {
+            // TODO maybe showQuickPick from workspaceFolders
+            throw "Unable to determine active directory in a multi-root workspace. Please open some file and try again.";
+        }
+        const workspace = vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor.document.uri);
+        if (workspace === undefined) {
+            // TODO maybe showQuickPick from workspaceFolders
+            throw "Unable to determine active workspace directory for the currently open file.";
+        }
+        return workspace;
     }
-
-    vscode.window.showErrorMessage(
-        `${EXTENSION_NAME}: Unable to determine active directory in a multi-root workspace. Please open some file and try again.`
-    );
 }
 
 module.exports = { rebuildCtags };
